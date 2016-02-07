c8fdf7494b1df26cff90d0b8eaed2d0c41e761ac
Khushboo Banga <khushboo.banga13@gmail.com>
2016-02-07 14:40:20 +0530

    Demo 1 commit

---
+++ Demo1/app.js
@@ -0,0 +1,47 @@
+//MODULE
+var lieApp = angular.module('lieApp', ['ngRoute', 'ngResource']);
+
+//ROUTES
+lieApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'views/one.htm',
+        controller: 'oneController'     
+    })
+    
+    .when('/two', {
+        templateUrl: 'views/two.htm',
+        controller: 'twoController'      
+    })
+    
+    
+});
+
+
+//SERVICES
+lieApp.service('cityService',function() {
+    
+    this.city ="Boston , US";
+});
+
+//CONTROLLERS
+lieApp.controller('oneController', ['$scope', 'cityService', function ($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    })
+    
+}]);
+
+lieApp.controller('twoController', ['$scope', '$resource', 'cityService' , function ($scope, $resource, cityService) {
+    
+    $scope.city = cityService.city;
+    $scope.weatherAPI=
+        $resource("http://jsonplaceholder.typicode.com/posts")
+    
+    $scope.wResult = $scope.weatherAPI.get();
+    
+}]);
